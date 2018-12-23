81347d69ecdadc4a397f7be0e76f56788c45ce82
Leo Vujanić <leonard.vujanic@gmail.com>
2018-12-23 20:15:53 +0100

    Add - make get request

---
+++ code/interface-http/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	url := "http://google.com"
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(resp.Status)
+
+}
