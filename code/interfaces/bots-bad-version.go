bfb0bd6926cb8d9816f802733ab5fbc9cbe8f8f5
Leo Vujanić <leonard.vujanic@gmail.com>
2018-12-23 18:14:29 +0100

    Interfaces - add bad example (problem which needs to be solved)

---
+++ code/interfaces/bots-bad-version.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type englishBot struct{}
+
+type spanishBot struct{}
+
+func main() {
+
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreetingSpanish(sb)
+
+}
+
+func (eb englishBot) getGreeting() string {
+	return "Hi There!"
+}
+
+func (sb spanishBot) getGreeting() string {
+	return "Hola!"
+}
+
+func printGreeting(eb englishBot) {
+	fmt.Println(eb.getGreeting())
+}
+
+func printGreetingSpanish(sb spanishBot) {
+	fmt.Println(sb.getGreeting())
+}
